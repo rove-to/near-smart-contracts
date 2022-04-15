a23695c96c91dce193f9b2cd05b6c04d93725b36
Khoa TM <tranminhkhoa1402@gmail.com>
2022-04-15 12:58:40 +0700

    init integrate Jest test

---
+++ tests/integration/App-integration.test.js
@@ -0,0 +1,25 @@
+// these are made available by near-cli/test_environment
+// note: do not remove the line below as it is needed for these tests
+/* global nearlib, nearConfig */
+
+// import 'regenerator-runtime/runtime';
+
+// let near;
+// let contract;
+// let accountId;
+
+beforeAll(async function() {
+  // near = await nearlib.connect(nearConfig);
+  // accountId = nearConfig.contractName;
+  // contract = await near.loadContract(nearConfig.contractName, {
+  //   viewMethods: ['view_nft'],
+  //   changeMethods: ['create_nft'],
+  //   sender: accountId
+  // });
+});
+
+
+it('test testcase', async() => {
+  expect(1).toEqual(1);
+});
+
