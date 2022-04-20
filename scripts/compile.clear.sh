0e9aef73f1ab5a51a48f90c48b63ad8eeeec81fe
thaibao-incognito <thaibao@autonomous.nyc>
2022-04-20 12:05:08 +0700

    - update

--- scripts/compile.clear.sh
+++ scripts/compile.clear.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 rm -rf ./compilers
-rm -rf ./contracts/goods/environments/target
+
+rust_files=$(find ./*/** -type f -name "lib.rs")
+for i in $rust_files; do
+  dir_path=$(dirname "$i")
+  parentdir="$(dirname "$dir_path")"
+  rm -rf $parentdir/target
+  echo "rm -rf $parentdir/target"
+done
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-rm -rf ./contracts/goods/environments/target
-sh ./contracts/goods/environments/build.sh
+
+rust_files=$(find ./*/** -type f -name "lib.rs")
+for i in $rust_files; do
+  dir_path=$(dirname "$i")
+  parentdir="$(dirname "$dir_path")"
+  echo $parentdir
+
+  rm -rf $parentdir/target
+  echo "rm -rf $parentdir/target"
+  sh $parentdir/build.sh
+  echo "run build $parentdir/build.sh"
+done
