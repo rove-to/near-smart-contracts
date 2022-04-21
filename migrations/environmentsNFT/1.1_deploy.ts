1af47f852f129011f38bd07d94e59a1261b82a5c
Dac Do Dinh <dacdodinh99@gmail.com>
2022-04-21 08:40:58 +0700

    update deploy script

--- migrations/environmentsNFT/1.1_deploy.ts
+++ migrations/environmentsNFT/1.1_deploy.ts
@@ -18,9 +18,9 @@ import * as fs from "fs";
         const tokenMetadataFile = process.argv[4];
         const tokenMetadata = JSON.parse((await fs.readFileSync(tokenMetadataFile)).toString());
         console.log(process.env);
-        const adminId = process.env.ADMIN_ID || "";
-        const operatorId = process.env.OPERATOR_ID || "";
-        const treasuryId = process.env.TREASURY_ID || "";
+        const adminId = process.argv[4] || process.env.ADMIN_ID || "";
+        const operatorId = process.argv[5] || process.env.OPERATOR_ID || "";
+        const treasuryId = process.argv[6] || process.env.TREASURY_ID || "";
         console.log(tokenMetadata);
         await nft.deploy(wasm, contractAccountId, 0, tokenMetadata, adminId, operatorId, treasuryId);
         console.log("Deployed contract on contractAccountId:", contractAccountId)
